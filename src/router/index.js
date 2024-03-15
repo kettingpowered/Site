48b70d41a48768886e83afa42ec6051fe133337f
JustRed23 <manuel.bormans@gmail.com>
2024-03-15 16:44:32 +0100

    Add base files

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/faq',
+      name: 'FAQ',
+      component: () => import('../views/Faq.vue')
+    },
+    {
+      path: '/download',
+      name: 'Download',
+      component: () => import('../views/Download.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: () => import('../views/NotFound.vue')
+    }
+  ]
+})
+
+export default router
