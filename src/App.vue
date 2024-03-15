a86011eae9f1bea1b84ed14e3074b5dabb98142b
JustRed23 <manuel.bormans@gmail.com>
2024-03-15 17:01:30 +0100

    Add basic navbar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    |
+    <RouterLink :to="{ name: 'FAQ' }">FAQ</RouterLink>
+    |
+    <RouterLink :to="{ name: 'Download' }">Download</RouterLink>
+  </nav>
   <RouterView />
 </template>
 
