8097a1ba170b1be46477f57b505c150dc79f83e7
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-03-28 22:57:03 +0100

    Use better js chunking

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, splitVendorChunkPlugin} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 
@@ -11,10 +11,22 @@ export default defineConfig({
       extensions: ['vue', 'md'],
     }),
     vue(),
+    splitVendorChunkPlugin(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output:{
+        manualChunks(id) {
+          if (id.includes("@fortawesome/free-solid-svg-icons")) return "fas";
+          if (id.includes("@fortawesome/free-regular-svg-icons")) return "far";
+          if (id.includes("@fortawesome/free-brands-svg-icons")) return "fab";
+        }
+      }
     }
   },
   ssgOptions: {
