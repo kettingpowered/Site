7f1f5ae66218d7d6d0f2fcc9bd20d359441c0bd1
JustRed23 <manuel.bormans@gmail.com>
2024-03-29 05:33:19 +0100

    Remove unusual file padding

--- src/main.js
+++ src/main.js
@@ -14,62 +14,60 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab)
 
 export const createApp = ViteSSG(
-  // the root component
-  App,
-  // vue-router options
-  {
-      routes: [
-          {
-              path: '/',
-              name: 'Home',
-              component: () => import('@/views/Home.vue')
-          },
-          {
-              path: '/faq',
-              name: 'FAQ',
-              component: () => import('@/views/Faq.vue')
-          },
-          {
-              path: '/team',
-              name: 'Team',
-              component: () => import('@/views/Team.vue')
-          },
-          {
-              path: '/download',
-              name: 'Download',
-              component: () => import('@/views/Download.vue')
-          },
-          {
-              path: '/404',
-              name: 'NotFound',
-              component: () => import('@/views/NotFound.vue')
-          }
-      ],
-      base: import.meta.env.BASE_URL,
-  },
-  // function to have custom setups
-  ({ app, router, routes, isClient, initialState }) => {
+    // the root component
+    App,
+    // vue-router options
+    {
+        routes: [
+            {
+                path: '/',
+                name: 'Home',
+                component: () => import('@/views/Home.vue')
+            },
+            {
+                path: '/faq',
+                name: 'FAQ',
+                component: () => import('@/views/Faq.vue')
+            },
+            {
+                path: '/team',
+                name: 'Team',
+                component: () => import('@/views/Team.vue')
+            },
+            {
+                path: '/download',
+                name: 'Download',
+                component: () => import('@/views/Download.vue')
+            },
+            {
+                path: '/404',
+                name: 'NotFound',
+                component: () => import('@/views/NotFound.vue')
+            }
+        ],
+        base: import.meta.env.BASE_URL,
+    },
+    // function to have custom setups
+    ({ app, router, routes, isClient, initialState }) => {
         app.component('font-awesome-icon', FontAwesomeIcon)
         //Create some global properties
         app.config.globalProperties.global = {
-          discordUrl: "https://discord.kettingpowered.org/",
-          githubUrl: "https://github.com/kettingpowered/",
-          downloadUrl: "https://nexus.c0d3m4513r.com/custom/forge-ketting-versions.xml",
-          softwareUrl: function() {
-              return this.githubUrl + "Ketting-1-20-x/";
-          },
-          unsupportedVersions: ["1.20.3"]
+            discordUrl: "https://discord.kettingpowered.org/",
+            githubUrl: "https://github.com/kettingpowered/",
+            downloadUrl: "https://nexus.c0d3m4513r.com/custom/forge-ketting-versions.xml",
+            softwareUrl: function() {
+                return this.githubUrl + "Ketting-1-20-x/";
+            },
+            unsupportedVersions: ["1.20.3"]
         };
 
         //Discord Roles and their colors
         app.config.globalProperties.discordRoles = {
-          "Owner": "#660000",
-          "Admin": "#ffcc00",
-          "Staff": "#ff6633",
-          "Helper": "#ff3333",
-          "Contributor": "#33cc99",
+            "Owner": "#660000",
+            "Admin": "#ffcc00",
+            "Staff": "#ff6633",
+            "Helper": "#ff3333",
+            "Contributor": "#33cc99",
         }
-
-      // install plugins etc.
-  }
+    }
 )
